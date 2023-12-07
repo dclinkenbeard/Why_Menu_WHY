bf0671bd6cb2c3a1deac7a97cb0036ac0b0066cd
Dr. Drew Clinkenbeard <dclinkenbeard@csumb.edu>
2023-12-07 12:02:32 -0800

    Added a toggle to show/hide a menu item

--- app/src/main/java/com/daclink/menu/MainActivity.java
+++ app/src/main/java/com/daclink/menu/MainActivity.java
@@ -6,14 +6,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.daclink.menu.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean isVisible = true;
+    Button button;
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        button = binding.ButtonMenuShow;
 
         invalidateOptionsMenu();
 
@@ -24,6 +39,24 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public void menuItemToggle(View view){
+        isVisible = !isVisible;
+        Toast.makeText(this, "Visible == " + isVisible, Toast.LENGTH_SHORT).show();
+        if(isVisible){
+            button.setText(R.string.here);
+        }else{
+            button.setText(R.string.not_here);
+        }
+        invalidateOptionsMenu();
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item = menu.findItem(R.id.showHide);
+        item.setVisible(isVisible);
         return true;
     }
 }
